2090bcbc2b5ac1330a176f46fc37930229dc10e7
yooyoung-mo <gigamadness@gmail.com>
2017-01-09 15:32:54 +0900

    최초 짝 프로그래밍

--- src/main/java/aep/AppConfig.java
+++ src/main/java/aep/AppConfig.java
@@ -1,7 +1,57 @@
 package aep;
 
+import net.sf.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by 유영모 on 2017-01-09.
  */
 public class AppConfig {
+    private static final String CONFIG_ROOT_DEFAULT_FILE_PATH = "conf/app-config.json";
+    private static final String CONFIG_PROFILE_ELEMENT = "profile";
+    private static final String SYSTEM_PROPERTY_APP_ENV_PROFILE_ACTIVE = "app.env.profile.active";
+
+    public static Map<String, String> get(String id) throws IOException {
+        InputStream configFile = getAppConfigFile();
+        JSONObject configJson = convertToJson(configFile);
+
+        JSONObject profile = (JSONObject)configJson.get(CONFIG_PROFILE_ELEMENT);
+        String appEnvProfileActive = System.getProperty(SYSTEM_PROPERTY_APP_ENV_PROFILE_ACTIVE);
+        JSONObject dev = (JSONObject)profile.get(appEnvProfileActive);
+        JSONObject contents = (JSONObject)dev.get(id);
+
+
+        Map<String, String> configMap = new HashMap<String, String>();
+        Set<Map.Entry<String, String>> set = contents.entrySet();
+        for(Map.Entry<String, String> o : set) {
+            configMap.put(o.getKey(), o.getValue());
+        }
+        return configMap;
+    }
+
+    private static JSONObject convertToJson(InputStream configFile) throws IOException {
+        BufferedReader bR = new BufferedReader(new InputStreamReader(configFile));
+        String line = "";
+
+        StringBuilder responseStrBuilder = new StringBuilder();
+        while ((line = bR.readLine()) != null) {
+
+            responseStrBuilder.append(line);
+        }
+        configFile.close();
+
+        return JSONObject.fromObject(responseStrBuilder.toString());
+
+    }
+
+    private static InputStream getAppConfigFile() {
+        return AppConfig.class.getClassLoader().getResourceAsStream(CONFIG_ROOT_DEFAULT_FILE_PATH);
+    }
 }
--- src/test/java/aep/AppConfigTest.java
+++ src/test/java/aep/AppConfigTest.java
@@ -1,7 +1,62 @@
 package aep;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * Created by 유영모 on 2017-01-09.
  */
 public class AppConfigTest {
+
+    @Test
+    public void get_개발프로파일() throws IOException {
+        // given
+        System.setProperty("app.env.profile.active", "dev");
+
+        // when
+        Map<String, String> configMap = AppConfig.get("paymentGateway");
+
+        // then
+        Assert.assertNotNull(configMap);
+        String domain = configMap.get("domain");
+
+        Assert.assertEquals("http://dev-pg.com", domain);
+    }
+
+    @Test
+    public void get_운영프로파일() throws IOException {
+        // given
+        System.setProperty("app.env.profile.active", "prod");
+
+        // when
+        Map<String, String> configMap = AppConfig.get("paymentGateway");
+
+        // then
+        Assert.assertNotNull(configMap);
+        String domain = configMap.get("domain");
+
+        Assert.assertEquals("http://pg.com", domain);
+    }
+
+    @Test
+    public void get_개발프로파일_propertyPath() throws IOException {
+        // given
+        System.setProperty("app.env.profile.active", "dev");
+
+        // when
+        Map<String, String> configMap = AppConfig.get("paymentGateway");
+
+        // then
+        Assert.assertNotNull(configMap);
+        String domain = configMap.get("propertyPath");
+
+        Assert.assertEquals("classpath:conf/dev-pg.properties", domain);
+    }
+
+
+
+
 }
