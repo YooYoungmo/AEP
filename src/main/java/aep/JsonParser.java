b58c7e1df11ccd43aca9cb9361ba255f9f65c382
yooyoung-mo <gigamadness@gmail.com>
2017-02-03 13:53:01 +0900

    https://github.com/YooYoungmo/AEP/issues/23 이슈 해결

--- src/main/java/aep/JsonParser.java
+++ src/main/java/aep/JsonParser.java
@@ -7,8 +7,14 @@ import java.util.Map;
 /**
  * Created by yooyoung-mo on 2017. 1. 10..
  */
-public class JsonParser {
-    public Map parse(String json) {
+public final class JsonParser {
+
+    /**
+     * 문자열을 Map 형태로 전환한다.
+     * @param json Json 문자열
+     * @return Map
+     */
+    public Map parse(final String json) {
         return JSONObject.fromObject(json);
     }
 }
---
+++ src/main/java/aep/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * AEP Root Package
+ * Created by yooyoung-mo on 2017. 2. 3..
+ */
+package aep;
