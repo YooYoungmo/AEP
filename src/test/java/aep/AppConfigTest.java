76ac1bfa7e7f5b91b21617b120f464e5f01b57b8
langve <langve0+github@gmail.com>
2017-02-22 16:56:26 +0900

    maven Junit 4.12 라이브러리 추가

--- src/test/java/aep/AppConfigTest.java
+++ src/test/java/aep/AppConfigTest.java
@@ -1,7 +1,24 @@
 package aep;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by 유영모 on 2017-01-09.
  */
 public class AppConfigTest {
+    @Test
+    public void getConfigValue()
+    {
+        // given
+        // when
+        AppConfigMap configMap = AppConfig.getConfigValue("paymentGateway");
+        String domain = configMap.getConfigValue("domain");
+        String propertyPath = configMap.getConfigValue("propertyPath");
+
+        // then
+        String expected = "classpath:conf/dev-pg.properties";
+        Assert.assertEquals(expected, propertyPath);
+
+    }
 }
